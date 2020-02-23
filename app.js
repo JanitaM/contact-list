11fea3b1b1a1c533e40a94e1f09c5dcc60174263
Janita M <48458664+JanitaM@users.noreply.github.com>
2020-02-22 20:07:41 -0500

    added a search input and edit button. worked on search function

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const
   phone = document.getElementById("phone"),
   email = document.getElementById("email"),
   submitBtn = document.getElementById("submit-btn"),
-  contacts = document.getElementById("contact-list");
+  contacts = document.getElementById("contact-list"),
+  searchInput = document.getElementById("search");
 
 // Class Constructors
 // Contact List
@@ -22,12 +23,10 @@ const addContact = e => {
   if (name.value === "" || phone.value === "" || email.value === "") {
     alertMessage("Please Enter All Values", "error");
   } else {
-    // instantiate and save to contact variable
+    // Instantiate and save to contact variable
     const contact = new Contact(name.value, phone.value, email.value);
-
     // Pass variable to function that displays on the ui
     addContactToList(contact);
-
     // Clear inputs after adding to ui
     clearInputs();
   }
@@ -40,12 +39,15 @@ const addContactToList = contact => {
   const contactList = document.getElementById("contact-list");
   // Create a table row
   listItem = document.createElement('tr');
+  // Class name to help with search function
+  listItem.className = "list-item";
   // Create the table data
   listItem.innerHTML = `
     <td>${contact.name}</td>
     <td>${contact.phone}</td>
     <td>${contact.email}</td>
     <td><button class="delete-btn">Delete</button></td>
+    <td><button class="edit-btn">Edit</button></td>
   `;
   // Append data to contact list
   contactList.appendChild(listItem);
@@ -77,17 +79,53 @@ const clearInputs = () => {
   email.value = "";
 }
 
-// delete btn doesn't exist until I add a contact..so target contact list
-const deleteContact = (e) => {
+// eventually, connect backend
+const deleteContact = e => {
+  // Target contact list
   if (e.target.className === "delete-btn") {
     e.target.parentElement.parentElement.remove();
   }
-
+  // Show success message
   alertMessage("Contact Removed", "success");
 
   e.preventDefault();
 }
 
+// eventually, connect to backend
+const editContact = e => {
+  // Target contact list?
+  if (e.target.className === "edit-btn") {
+    // e.target.parentElement.parentElement.remove();
+    // do something
+    console.log(`edit btn`)
+  }
+  // Show sucess message
+  alertMessage("Contact Updated", "success");
+
+  e.preventDefault();
+}
+
+// eventually, connect to backend
+// use filter instead?
+const filterContacts = e => {
+  // Search input value 
+  const text = e.target.value.toLowerCase();
+  // Each tr has a class of list-item
+  document.querySelectorAll(".list-item").forEach(contact => {
+    let item = contact.firstChild.nextSibling.innerHTML;
+    // index.of returns -1 if it's NOT a match
+    if (item.toLowerCase().indexOf(text) == -1) {
+      // Hide it
+      contact.style.display = "none";
+    } else {
+      // Show it
+      contact.style.display = "table-row";
+    }
+  });
+}
+
 // Event Listener
 submitBtn.addEventListener("click", addContact);
-contacts.addEventListener("click", deleteContact);
+contacts.addEventListener("click", deleteContact);  //connects to backend
+searchInput.addEventListener("keyup", filterContacts);
+contacts.addEventListener("click", editContact); //connects to backend
